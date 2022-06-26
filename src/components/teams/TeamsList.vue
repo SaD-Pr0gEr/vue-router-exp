9f99fe6a0db749d2b99d0612801a75680fe82e34
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-06-26 20:50:57 +0500

    added redirect and other components

---
+++ src/components/teams/TeamsList.vue
@@ -0,0 +1,36 @@
+<template>
+    <button class="confirm" @click="confirmRedirect"><span>confirm redirect to users</span></button>
+    <ul>
+        <teams-item
+            v-for="team in teams"
+            :key="team.id"
+            :name="team.name"
+            :member-count="team.members.length"
+        ></teams-item>
+    </ul>
+</template>
+
+<script>
+import TeamsItem from './TeamsItem.vue';
+
+export default {
+    components: {
+        TeamsItem,
+    },
+    inject: ['teams'],
+    methods: {
+        confirmRedirect() {
+            this.$router.push("/users")
+        }
+    }
+};
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    max-width: 40rem;
+    padding: 0;
+}
+</style>
---
+++ src/components/users/UsersList.vue
@@ -0,0 +1,46 @@
+<template>
+    <button class="confirm" @click="confirmRedirect"><span>confirm redirect to teams</span></button>
+    <ul class="users_list">
+        <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    </ul>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+    components: {
+        UserItem,
+    },
+    inject: ['users'],
+    methods: {
+        confirmRedirect () {
+            this.$router.push("/teams")
+        }
+    }
+};
+</script>
+
+<style>
+.users_list {
+    list-style: none;
+    margin: 2rem auto;
+    max-width: 20rem;
+    padding: 0;
+}
+
+.confirm {
+    background-color: #ff4f4f;
+    color: white;
+    padding: 15px;
+    border-radius: 5px;
+    border: #ff4f4f;
+    margin-left: 50px;
+    font-size: 15px;
+}
+
+.confirm:hover {
+    background-color: #f1a80a;
+    color: #252525;
+}
+</style>
